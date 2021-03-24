91449934059fe856a3821d0c42c692bae70ea854
Connor Smith <connorsmith78731@utexas.edu>
2021-03-23 21:17:33 -0500

    chaning init

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@ import os
 import sys
 from flask import Flask
 from flaskr import create_app
+from flaskr import static, templates
+import flaskr
 if __name__ == "__main__":
     app = create_app()
     app.run()
